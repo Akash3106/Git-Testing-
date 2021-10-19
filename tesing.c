4208ac232a692da1f53ab304a9ffcd925d9fa3ad
Akash3106 <akash.sharma3106@gmail.com>
2021-10-19 15:04:22 +0530

    merge test

--- tesing.c
+++ tesing.c
@@ -2,5 +2,7 @@
 
 void main(){
 	printf("This is a First Edit by Akash ");
-	printf("This is a commit for different Branch 	
+	printf("This is a commit for different Branch");
+	print("This is a merge test");
+	return 0; 	
 }
